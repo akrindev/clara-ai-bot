71825a9051ee5f3b42f58d76a8a68e55a85970ec
akrindev <akrinmin@gmail.com>
2023-09-26 18:20:48 +0700

    Add logging for context in issues.opened event

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const probotApp = (app) => {
   app.log("Yay! The app was loaded!");
 
   app.on("issues.opened", async (context) => {
+    console.log(context);
     return context.octokit.issues.createComment(
       context.issue({ body: "Hello, World!" })
     );
